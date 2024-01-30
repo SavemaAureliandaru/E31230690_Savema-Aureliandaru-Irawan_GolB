d6c1f743a3139c97c92dd9dc83fe66ce419fba7f
Savema <117051936+Savema@users.noreply.github.com>
2024-01-30 07:48:05 +0700

    Create suhu.java

---
+++ suhu.java
@@ -0,0 +1,23 @@
+package acara9;
+
+public class suhu {
+
+    public static void main(String[] args) {
+        
+        int suhu = 36;
+        
+        if (suhu == 38) {
+            System.out.println("Suhu anda panas");
+        }
+            
+        else if (suhu == 36 ) {
+            System.out.println("Suhu anda normal");
+        }
+        
+        else if (suhu == 35 ) {
+            System.out.println("Suhu anda dingin");
+        }
+        
+    }
+    
+}
